db7dca7abbdc7829ec80602ec29b1d41acfbc95e
student <stu@ravtech.local>
2021-05-25 12:33:22 +0300

    ...

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { WebcamImage } from 'ngx-webcam';
 import { Intern } from '../models/intern';
 import { UsersService } from './users.service';
 
@@ -8,7 +7,7 @@ import { UsersService } from './users.service';
 })
 export class RegisterService {
 
-  user: Intern = { id: 0, firstName: '', surName: '', passport: 0, telephone: '' }
+  user: Intern = { id: 0, firstName: '', surName: '', passport: 0, telephone: '' };
   constructor(private usersService: UsersService) { }
   registerUser() {
     this.usersService.addUser(this.user);
