25c323ee457f14ef184d5df696c9b197c61dc59d
student <stu@ravtech.local>
2021-05-27 15:33:16 +0300

    login added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/services/register.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  passport = '';
+  constructor(private usersService: UsersService, private registerService: RegisterService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.usersService.getUsers().subscribe(users => {
+      let user = users.find(user => user.passport == +this.passport)
+      if (user) {
+        this.registerService.user = user;
+        this.router.navigate(["/register2"]);
+      }
+      else {
+        alert("No user found");
+      }
+    });
   }
 
 }
