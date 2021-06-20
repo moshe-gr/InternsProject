a0b9df35d4f44519cb91f72ad667ed7dce887000
moshe-gr <mosegreenberg6@gmail.com>
2021-06-20 13:23:29 +0300

    ///

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Role } from 'src/app/enums/role.enum';
 import { AuthService } from 'src/app/services/auth.service';
 import { CurrentUserService } from 'src/app/services/currentUser.service';
 
@@ -12,9 +13,11 @@ import { CurrentUserService } from 'src/app/services/currentUser.service';
 export class LoginComponent implements OnInit {
 
   passport: number;
+  userRole: string;
 
   constructor(private authService: AuthService, private currentUserService: CurrentUserService, private router: Router, private modalService: NgbModal, config: NgbModalConfig) { 
     config.backdrop = 'static';
+    this.userRole = Role[this.currentUserService.user.role_number];
   }
 
   ngOnInit(): void {
